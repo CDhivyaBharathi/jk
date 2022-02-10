7f9c56e171281adf3f51bef1b6d3aac8205d8d39
C Dhivya Bharathi <bharathi.chellakumar@gmail.com>
2022-02-10 08:27:18 +0530

    commit

---
+++ Look at me/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './Screens/mainScreen';
+
+
+export default function App() {
+  return (
+    <MainScreen/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
